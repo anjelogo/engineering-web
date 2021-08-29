b65071db1cf5d2fc6cac7c0af88335c24570a0f2
Deorc <deorcstudios@gmail.com>
2021-08-29 03:11:14 -0700

    Update readme, header

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,35 +85,7 @@ module.exports = {
            'error' : '#ff5724',             /* Error */
         }
 			},
-			{
-				"cyberpunk": {
-					"fontFamily": "ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace",
-					"primary": "#ff7598",
-					"primary-focus": "#ff5784",
-					"primary-content": "#000000",
-					"secondary": "#75d1f0",
-					"secondary-focus": "#5abfdd",
-					"secondary-content": "#000000",
-					"accent": "#c07eec",
-					"accent-focus": "#af59e8",
-					"accent-content": "#000000",
-					"neutral": "#423f00",
-					"neutral-focus": "#090901",
-					"neutral-content": "#ffee00",
-					"base-100": "#ffee00",
-					"base-200": "#dbcd00",
-					"base-300": "#b8ab00",
-					"base-content": "#000000",
-					"info": "#2094f3",
-					"success": "#009485",
-					"warning": "#ff9900",
-					"error": "#ff5724",
-					"--rounded-box": "0",
-					"--rounded-btn": "0",
-					"--rounded-badge": "0",
-					"--tab-radius": "0"
-				}
-			}
+			"cyberpunk"
     ],
   },
 }
