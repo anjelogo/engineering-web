2086f83cff0b745b52a6ece1aae3433af818b4ed
Deorc <deorcstudios@gmail.com>
2021-08-30 01:16:04 -0700

    Revert "Add support for navbar fade-in"
    This reverts commit cf7cf98e3b5ec5be0faff2a1324edd5fae668f46.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,14 +2,7 @@ import React from "react";
 import Footer from "./footer";
 import Navbar from "./header";
 
-import { useEffect } from 'react';
-import {themeChange} from "theme-change"
-
 const Layout = ({ children }): JSX.Element => {
-
-	useEffect(() => {
-		themeChange(false)
-	}, []);
 
 	return (
 		<html>
