ba3c1e174ccf322879516e3ffec1a0e35f852720
Deorc <deorcstudios@gmail.com>
2021-09-12 22:17:47 -0700

    New SVG for theme changer, Replace Logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import logo from "../public/ec.png"
-import logo2 from "../public/ectrans.png"
 import Link from "next/link";
-import Image from "next/image"
 
 interface NavbarProps {
 	children: React.ReactNode
@@ -65,10 +62,7 @@ export default class Navbar extends React.Component<NavbarProps, NavbarStates> {
 							
 							{/* DESKTOP LOGO */}
 
-							<div className="hidden navbar-start md:flex px-2 mx-2" id="toggle">
-								<Image src={logo} alt="logo" width={50} height={50} className="darktheme"/>
-								<Image src={logo2} alt="logo" width={50} height={50} className="lighttheme"/>
-							</div>
+							<p className="hidden navbar-start md:flex px-2 mx-2 font-abril text-2xl text-primary">EC</p>
 
 							{/* MOBILE MENU */}
 
@@ -95,10 +89,7 @@ export default class Navbar extends React.Component<NavbarProps, NavbarStates> {
 
 							{/* MOBILE LOGO */}
 
-							<div className="md:hidden navbar-center flex px-2 mx-2" id="toggle">
-								<Image src={logo} alt="logo" width={50} height={50} className="darktheme"/>
-								<Image src={logo2} alt="logo" width={50} height={50} className="lighttheme"/>
-							</div>
+							<p className="md:hidden navbar-center px-2 mx-2 font-abril text-2xl text-primary">EC</p>
 
 							{/* DESKTOP LINKS */}
 
@@ -125,9 +116,12 @@ export default class Navbar extends React.Component<NavbarProps, NavbarStates> {
 
 							<div className="navbar-end">
 								<div className="flex-none" id="toggle">
-									<button data-toggle-theme="lighttheme,darktheme" data-act-class="ACTIVECLASS" className="btn btn-square btn-primary-content">
-										<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+									<button data-toggle-theme="darktheme,lighttheme" data-act-class="ACTIVECLASS" className="btn btn-square btn-primary-content">
+										<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 darktheme" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+										</svg>
+										<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 lighttheme" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
 										</svg>
 									</button>
 								</div>
