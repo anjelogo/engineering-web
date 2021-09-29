7089e312200f00f1c091b5ee0ea2e73b6378e5c3
Anjelo Go <30453402+anjelogo@users.noreply.github.com>
2021-09-29 08:02:26 -0700

    Update ID strings

--- lib/getIds.ts
+++ lib/getIds.ts
@@ -1,7 +1,7 @@
 export default function getIDs(): string[] {
 	let arr;
 
-	const string = process.env.IDS ?? "6153aa2b6e211f0008453dfa:6153aa2b6e211f0008453dfa";
+	const string = process.env.IDS ?? "6153aa2b6e211f0008453dfa:61547f0de64aca0009b9bfd6";
 
 	if (string.length && string.includes(":")) {
 		arr = string.split(":");
