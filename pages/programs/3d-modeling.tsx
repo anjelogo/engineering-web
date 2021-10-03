d0b08c69241423d7644162dbe1680850b47c8367
Deorc <deorcstudios@gmail.com>
2021-10-02 19:57:59 -0700

    Remove Alert for 3D Modeling Page

--- pages/programs/3d-modeling.tsx
+++ pages/programs/3d-modeling.tsx
@@ -46,16 +46,6 @@ const ModelingProgram = (): JSX.Element => {
 				title="3D MODELING"
 				subtitle="3D Printer go brr"
 				background="bg-3dmodeling"
-				alerts={(
-					<div className="alert alert-error mb-5 m-10">
-						<div className="flex-1">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6 mx-2 stroke-current"> 
-								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>                         
-							</svg> 
-							<label>This program is currently suspended. Please check back later.</label>
-						</div>
-					</div>
-				)}
 				body={
 					[
 						(
