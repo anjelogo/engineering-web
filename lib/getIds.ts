0ff2dc4f920e42b4a89c29abc5b1f09635f86a5f
Anjelo Go <30453402+anjelogo@users.noreply.github.com>
2021-11-05 11:16:49 -0700

    Add Alijah ID

--- lib/getIds.ts
+++ lib/getIds.ts
@@ -3,6 +3,7 @@ export default function getIDs(): string[] {
 		"6153aa2b6e211f0008453dfa", //Anjelo
 		"615526f55809c40009441f11", //Alex
 		"615527a15809c40009441f14", //Alisa
-		"61547f0de64aca0009b9bfd6" // Orlando
+		"61547f0de64aca0009b9bfd6", // Orlando
+		"6171e2bfc3c5620009c9dee1" //Alijah
 	];
 }
